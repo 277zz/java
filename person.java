4fcd8ae83bad4d3e3b09606c0a181fd269c00391
277zz <73331084+277zz@users.noreply.github.com>
2020-10-23 15:06:28 +0800

    Create person.java
    java.person

---
+++ person.java
@@ -0,0 +1,46 @@
+class person {
+class Person{
+private String name;
+
+private String sex;
+
+private int age;
+
+public Person()
+
+{
+           name = null;
+           sex = null;.
+           age = 0;
+}
+
+    public void setName(String n)
+    
+    {
+     name = n;
+    }
+    
+    
+    public void setSex(String s)
+    
+    
+    {
+     sex = s;
+    }
+    
+    public void setAge(String a)
+    
+    {
+     age = a;
+    
+    }
+    public void print()
+    
+    
+    {
+     System.out.println("name:"+name+"")
+    }
+
+
+
+}
